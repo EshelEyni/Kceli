1d084b6252741a65744518cec1cfa6bb42ce117d
Eshel Eyni <esheleyni@gmail.com>
2023-10-07 08:28:11 +0300

    build: Query Nutrition, Day Edit Refactoring - Fixed Imports

--- backend/build/models/day/intakeSchema.js
+++ backend/build/models/day/intakeSchema.js
@@ -1,107 +1,134 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.intakeSchema = void 0;
 const mongoose_1 = require("mongoose");
-const intake_1 = require("../../../../shared/types/intake");
+const intake_1 = require("../../../../shared/dist/types/intake");
 const openAIService_1 = __importDefault(require("../../services/openAI/openAIService"));
 const intakeService_1 = __importDefault(require("../../services/intake/intakeService"));
-const intakeItemSchema = new mongoose_1.Schema({
+const intakeItemSchema = new mongoose_1.Schema(
+  {
     name: {
-        type: String,
-        required: [true, "Please provide a name"],
+      type: String,
+      required: [true, "Please provide a name"],
     },
     unit: {
-        type: String,
-        enum: intake_1.MeasurementUnit,
-        required: [true, "Please provide a unit"],
+      type: String,
+      enum: intake_1.MeasurementUnit,
+      required: [true, "Please provide a unit"],
     },
     quantity: {
-        type: Number,
-        required: [true, "Please provide a quantity"],
+      type: Number,
+      required: [true, "Please provide a quantity"],
     },
     calories: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
-}, {
+  },
+  {
     toObject: {
-        virtuals: true,
-        transform: (_, ret) => {
-            delete ret._id;
-            return ret;
-        },
+      virtuals: true,
+      transform: (_, ret) => {
+        delete ret._id;
+        return ret;
+      },
     },
     toJSON: {
-        virtuals: true,
-        transform: (_, ret) => {
-            delete ret._id;
-            return ret;
-        },
+      virtuals: true,
+      transform: (_, ret) => {
+        delete ret._id;
+        return ret;
+      },
     },
-});
+  }
+);
 intakeItemSchema.pre("validate", function (next) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const intakeItem = this.toObject();
-            if (intakeItem.calories)
-                return next();
-            const existingItemData = yield intakeService_1.default.getExistingIntakeItem(intakeItem);
-            if (existingItemData && existingItemData.calories) {
-                this.calories = existingItemData.calories * (intakeItem.quantity / existingItemData.quantity);
-            }
-            else {
-                this.calories = yield openAIService_1.default.getCaloriesForIntakeItem(intakeItem);
-            }
-        }
-        catch (error) {
-            // eslint-disable-next-line no-console
-            console.error(error);
-        }
-        finally {
-            next();
-        }
-    });
+  return __awaiter(this, void 0, void 0, function* () {
+    try {
+      const intakeItem = this.toObject();
+      if (intakeItem.calories) return next();
+      const existingItemData = yield intakeService_1.default.getExistingIntakeItem(intakeItem);
+      if (existingItemData && existingItemData.calories) {
+        this.calories =
+          existingItemData.calories * (intakeItem.quantity / existingItemData.quantity);
+      } else {
+        this.calories = yield openAIService_1.default.getCaloriesForIntakeItem(intakeItem);
+      }
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+    } finally {
+      next();
+    }
+  });
 });
-const intakeSchema = new mongoose_1.Schema({
+const intakeSchema = new mongoose_1.Schema(
+  {
     items: {
-        type: [intakeItemSchema],
-        required: [true, "Please provide intake items"],
+      type: [intakeItemSchema],
+      required: [true, "Please provide intake items"],
     },
     isRecorded: {
-        type: Boolean,
-        default: true,
+      type: Boolean,
+      default: true,
     },
     recordedAt: {
-        type: Date,
+      type: Date,
     },
-}, {
+  },
+  {
     toObject: {
-        virtuals: true,
-        transform: (_, ret) => {
-            delete ret._id;
-            return ret;
-        },
+      virtuals: true,
+      transform: (_, ret) => {
+        delete ret._id;
+        return ret;
+      },
     },
     toJSON: {
-        virtuals: true,
-        transform: (_, ret) => {
-            delete ret._id;
-            return ret;
-        },
+      virtuals: true,
+      transform: (_, ret) => {
+        delete ret._id;
+        return ret;
+      },
     },
-});
+  }
+);
 exports.intakeSchema = intakeSchema;
 // Path: src/models/day/dailyDataModel.ts
 //# sourceMappingURL=intakeSchema.js.map
